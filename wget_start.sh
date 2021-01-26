1ec0dca29fbdee23346397a50329274e6d260fbc
DakiGamingHD <38489619+DakiGamingHD@users.noreply.github.com>
2021-01-26 11:56:24 +0100

    creatr
    edit

--- wget_start.sh
+++ wget_start.sh
@@ -1,8 +1,14 @@
-@echo off
-Echo Universal IPTV Scanner Android v0.1
-cls
-color F1
-echo Type (copy and paste) the server address with its port and press Enter
+clear
+
+echo Universal IPTV Scanner Android v0.1
+
+echo Universal IPTV Scanner Android v0.1
+
+echo Universal IPTV Scanner Android v0.1
+
 echo.
+
 read server /p "Unesite server adresu sa portom i pritisnite Enter:  "
+
 wget  --base %server% -O Hit_Mac_list.txt -i combo.txt
+
