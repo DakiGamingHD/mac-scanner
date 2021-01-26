9c64fac48b4940e43025ce340f6e222aa2f6dc7d
DakiGamingHD <38489619+DakiGamingHD@users.noreply.github.com>
2021-01-26 12:10:19 +0100

    Create wget_start.sh

--- wget_start.sh
+++ wget_start.sh
@@ -2,6 +2,6 @@ clear
 echo Universal IPTV Scanner Android v0.1
 echo Universal IPTV Scanner Android v0.1
 echo Universal IPTV Scanner Android v0.1
-echo.
+
 read server -p "Unesite server adresu sa portom i pritisnite Enter:  "
-wget  --base %server% -O Hit_Mac_list.txt -i combo.txt
+wget  --base server -O Hit_Mac_list.txt -i combo.txt
